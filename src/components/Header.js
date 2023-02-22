66e87b29bc699eed8d12fc7177f8ab4658be9ecc
Esteban Lissandrello <elissandrello@andreani.com>
2023-02-21 22:11:35 -0300

    SaveAreaView

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 
-const Header = ({title, newStyle}) => {
+const Header = ({ title, newStyle }) => {
   return (
-    <View style={styles.header}>
-      <Text style={{...styles.headerTitle, ...newStyle}}>{title}</Text>
-    </View>
+    <SafeAreaView style={styles.header}>
+      <View>
+        <Text style={{ ...styles.headerTitle, ...newStyle }}>{title}</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Header
 
 const styles = StyleSheet.create({
-    header:{
-        backgroundColor: "#151522",
-        height: 90,
-        padding: 30,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    headerTitle:{
-        color: "white",
-        fontSize: 25,
-    }
+  header: {
+    backgroundColor: "#151522",
+    // height: 90,
+    // padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    color: "white",
+    fontSize: 25,
+  }
 })
--- src/screens/ResultScreens.js
+++ src/screens/ResultScreens.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
 import win from "../assets/images/Win.png"
 import lose from "../assets/images/Over.png"
 import Card from '../components/Card'
@@ -8,40 +8,42 @@ import Colors from '../constants/Colors'
 
 const ResultScreens = ({ result }) => {
 
-  const [image, setImage] = useState("")
+    const [image, setImage] = useState("")
 
-  useEffect(() => {
-    handleImage()
-  }, [])
+    useEffect(() => {
+        handleImage()
+    }, [])
 
-  const handleImage = () => {
-    if (result === "Win"){
-        setImage(win)
-        return
+    const handleImage = () => {
+        if (result === "Win") {
+            setImage(win)
+            return
+        }
+        setImage(lose)
     }
-    setImage(lose)
-  }
-  
-  return (
-    <View style={styles.container}>
-      <Card>
-        <Text>{`You ${result}`}</Text>
-      </Card>
-      <Image style={styles.imageContainer} source={image}/>
-    </View>
-  )
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Card>
+                    <Text>{`You ${result}`}</Text>
+                </Card>
+                <Image style={styles.imageContainer} source={image} />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default ResultScreens
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: Colors.primary,
     },
-    imageContainer:{
+    imageContainer: {
         height: 320,
         width: 320,
         marginTop: 50,
